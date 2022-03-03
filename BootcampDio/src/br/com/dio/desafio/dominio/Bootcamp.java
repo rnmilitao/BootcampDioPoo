190d359f7455e118b2c10296e5aa228b8b96c334
rnmilitao <92827183+rnmilitao@users.noreply.github.com>
2022-03-03 14:31:43 -0400

    Adicionei arquivos via upload

---
+++ BootcampDio/src/br/com/dio/desafio/dominio/Bootcamp.java
@@ -0,0 +1,65 @@
+package br.com.dio.desafio.dominio;
+
+import java.time.LocalDate;
+import java.util.HashSet;
+import java.util.LinkedHashSet;
+import java.util.Objects;
+import java.util.Set;
+
+public class Bootcamp {
+	private String nome;
+	private String descricao;
+	private final LocalDate dataInicial = LocalDate.now();
+	private final LocalDate dataFinal = dataInicial.plusDays(45);
+	private Set<Dev> devsInscritos = new HashSet<>();
+	private Set<Conteudo> conteudos = new LinkedHashSet<>();
+	
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getDescricao() {
+		return descricao;
+	}
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+	public LocalDate getDataInicial() {
+		return dataInicial;
+	}
+	public LocalDate getDataFinal() {
+		return dataFinal;
+	}
+	public Set<Dev> getDevsInscritos() {
+		return devsInscritos;
+	}
+	public void setDevsInscritos(Set<Dev> devsInscritos) {
+		this.devsInscritos = devsInscritos;
+	}
+	public Set<Conteudo> getConteudos() {
+		return conteudos;
+	}
+	public void setConteudos(Set<Conteudo> conteudos) {
+		this.conteudos = conteudos;
+	}
+	
+		@Override
+	public int hashCode() {
+		return Objects.hash(devsInscritos, conteudos,dataFinal, dataInicial, descricao, nome);
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Bootcamp bootcamp = (Bootcamp) obj;
+		return Objects.equals(dataFinal, bootcamp.dataFinal) && Objects.equals(dataInicial, bootcamp.dataInicial)
+				&& Objects.equals(descricao, bootcamp.descricao) && Objects.equals(nome, bootcamp.nome);
+	}
+
+}
